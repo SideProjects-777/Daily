4923bfce1acc497b856105db979407ccf9ae1534
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2023-03-28 18:26:01 +0200

    added date picker

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
-import {Alert, Modal,StyleSheet, Text, Pressable,View, TouchableOpacity} from 'react-native';
+import {Alert, Modal,StyleSheet, Text, Pressable,View, TouchableOpacity, TextInput} from 'react-native';
 import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
 import testIDs from './dataSet';
 import ActionButton from 'react-native-action-button';
+import DatePicker from 'react-native-date-picker';
 
 export default class App extends Component {
   state = {
     items: undefined,
     modalVisible: false,
+    name:'',
+    from:new Date(),
+    openFrom:false,
+    to:new Date(),
+    openTo:false,
+    day:'',
+    bio:'',
   };
 
    reservationsKeyExtractor = (item, index) => {
@@ -49,16 +57,67 @@ export default class App extends Component {
           visible={this.state.modalVisible}
           onRequestClose={() => {
             Alert.alert('Modal has been closed.');
-            this.setState({modalVisible: !this.state.modalVisible});
+            this.updateVisibility(this.state.changeVisibility);
           }}>
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => this.setState({modalVisible: !this.state.modalVisible})}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </Pressable>
+              <View style={styles.actionsInputs}>
+
+                <TouchableOpacity style={[styles.button, styles.buttonClose]}  onPress={() => this.setOpenFrom(true)}>
+                    <Text style={styles.buttonText}>From</Text>
+                  </TouchableOpacity>
+                  <DatePicker
+                    modal
+                    open={this.state.openFrom}
+                    date={this.state.from}
+                    onConfirm={(date) => {
+                      this.setOpenFrom(false)
+                      this.setFrom(date)
+                    }}
+                    onCancel={() => {
+                      this.setOpenFrom(false)
+                    }}
+                  />
+
+                <Text style={styles.label}>To</Text>
+                <TouchableOpacity style={[styles.button, styles.buttonClose]}  onPress={() => this.setOpenTo(true)}>
+                    <Text style={styles.buttonText}>Close</Text>
+                </TouchableOpacity>
+                  <DatePicker
+                      modal
+                      open={this.state.openTo}
+                      date={this.state.to}
+                      onConfirm={(date) => {
+                        this.setOpenTo(false)
+                        this.setTo(date)
+                      }}
+                      onCancel={() => this.setOpenTo(false)}
+                    />
+              </View>
+              <Text style={styles.label}>Name</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Name"
+                value={this.state.name}
+                onChangeText={this.setName}
+              />
+              <Text style={styles.label}>Description</Text>
+              <TextInput
+                style={styles.inputBig}
+                placeholder="Enter Description"
+                value={this.state.bio}
+                onChangeText={this.setBio}
+              />
+
+              <View style={styles.actions}>
+                  <TouchableOpacity style={[styles.button, styles.buttonClose]}  onPress={() =>{}}>
+                    <Text style={styles.buttonText}>Close</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={[styles.button, styles.buttonSave]}   onPress={() => this.updateVisibility(this.state.changeVisibility)}>
+                    <Text style={styles.buttonText}>Save</Text>
+                  </TouchableOpacity>
+              </View>
+
             </View>
           </View>
         </Modal>
@@ -67,13 +126,45 @@ export default class App extends Component {
           buttonColor='#9b59b6' 
           title="New Task" 
           useNativeDriver={false}
-          onPress={() => this.setState({modalVisible: !this.state.modalVisible})}
+          onPress={() => this.updateVisibility(this.state.changeVisibility)}
           >
             <Text>+</Text>
           </ActionButton.Item>
         </ActionButton>
       </View>
     );
+  }
+
+  setFrom = (val) => {
+    this.setState({from:val})
+  }
+
+  setOpenFrom = (val) => {
+    this.setState({openFrom:val})
+  }
+
+  setTo = (val) => {
+    this.setState({to:val})
+  }
+
+  setOpenTo = (val) => {
+    this.setState({openTo:val})
+  }
+
+  setName = (val) => {
+    this.setState({name:val})
+  }
+
+  setBio = (val) => {
+    this.setState({bio:val})
+  }
+
+  setEmail = (val) => {
+    this.setState({email:val})
+  }
+
+  updateVisibility = (val) =>{
+    this.setState({modalVisible:val})
   }
 
   loadItems = (day) => {
@@ -179,6 +270,8 @@ const styles = StyleSheet.create({
     marginTop: 22,
   },
   modalView: {
+    width: '90%',
+    minHeight:'20%',
     margin: 20,
     backgroundColor: 'white',
     borderRadius: 20,
@@ -212,5 +305,71 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: 'center',
-  }
+  },
+
+  //form
+  label: {
+    marginTop: 20,
+    marginBottom: 15,
+  },
+  input: {
+    width:'90%',
+    minHeight:'5%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    fontSize: 18,
+  },
+
+  inputBig: {
+    minHeight:'20%',
+    width:'90%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    fontSize: 18,
+  },
+
+  //buttons
+
+  actions: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderTopWidth:0.5,
+    borderTopColor:'#b3b3b3',
+    borderBottomWidth:0.5,
+    borderBottomColor:'#b3b3b3',
+    padding:40
+
+  },
+  button: {    
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft:10,
+  },
+  buttonSave:{
+    backgroundColor: '#17993a',
+  },  
+  buttonClose:{
+    backgroundColor: '#d90b2d',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonTextSave: {
+    color: '#fff',
+    fontSize: 16,
+  },
+
+  //mixed inputs
+
+  actionsInputs: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
