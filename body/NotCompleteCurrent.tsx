8ae11aadef84855456356cedce0293a09fc6f7df
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2023-04-24 22:19:08 +0200

    translated into typescript

---
+++ body/NotCompleteCurrent.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+type Props = {
+  data: {
+    start: string;
+    end: string;
+    name: string;
+    description: string;
+  };
+};
+
+const NotCompleteCurrent: FC<Props> = ({ data }) => {
+    return (
+      <>
+        <View
+          style={{
+            position: 'absolute',
+            alignItems: 'center',
+            justifyContent: 'center',
+            right: 10,
+            top: 10,
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              width: 120,
+              height: 45,
+              backgroundColor: '#00b9f7',
+              transform: [{ rotate: '45deg' }],
+            }}
+          />
+          <Text style={{ color: 'white', fontSize: 15, fontWeight: 'bold' }}>
+            <MaterialCommunityIcons name="update" color='white' size={15} />
+          </Text>
+        </View>
+        <Text style={{ fontSize: 18, color: 'black' }}>{data.start} - {data.end}</Text>
+        <Text style={{ fontSize: 16, color: 'black' }}>{data.name}</Text>
+        <Text style={{ fontSize: 14, color: 'black' }}>{data.description}</Text>
+      </>
+    );
+  };
+
+
+  export default NotCompleteCurrent;
+  
--- body/NotCompletePassed.tsx
+++ body/NotCompletePassed.tsx
@@ -2,39 +2,44 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+interface Props {
+  data: {
+    start: string;
+    end: string;
+    name: string;
+    description: string;
+  }
+}
 
-
-function NotCompleteCurrent(props) {
-    return (
-      <>
-          <View
-          style={{
+function NotCompletePassed(props: Props) {
+  return (
+    <>
+      <View
+        style={{
           position: 'absolute',
           alignItems: 'center',
           justifyContent: 'center',
           right: 10,
           top: 10,
-          }}>
-          <View
+        }}>
+        <View
           style={{
-          position: 'absolute',
-          width: 120,
-          height: 45,
-          backgroundColor: '#00b9f7',
-          transform: [{rotate: '45deg'}],
+            position: 'absolute',
+            width: 120,
+            height: 45,
+            backgroundColor: 'red',
+            transform: [{rotate: '45deg'}],
           }}
-      />
-      <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>
-          <MaterialCommunityIcons name="update" color='white' size={15} />
-      </Text>
+        />
+        <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>
+          <MaterialCommunityIcons name="bell" color='white' size={15} />
+        </Text>
       </View>
       <Text style={{ fontSize:18,color:'black'}}>{props.data.start} - {props.data.end}</Text>
       <Text style={{ fontSize:16,color:'black'}}>{props.data.name}</Text>
       <Text style={{ fontSize:14,color:'black'}}>{props.data.description}</Text>
     </>
-    );
-  }
-  
+  );
+}
 
-  
-  export default NotCompleteCurrent;
+export default NotCompletePassed;
