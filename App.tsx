3cc3267c851776c65a5fa3b6e346c36c81aa96f2
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2023-05-06 11:56:28 +0200

    #2 removed files

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './HomeScreen';
-import NewEvent from './NewEvent';
+import HomeScreen from './src/HomeScreen';
+import NewEvent from './src/NewEvent';
 
 const Stack = createStackNavigator();
 
